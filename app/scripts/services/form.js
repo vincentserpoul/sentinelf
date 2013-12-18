58ea4f5941f75bd608c18aec412013b310e0e815
Jean Jacques Serpoul <serpoul@gmail.com>
2013-12-19 00:55:00 +0100

    Add FormServiceForUpadte

--- app/scripts/services/form.js
+++ app/scripts/services/form.js
@@ -47,6 +47,51 @@ sentinelfApp.factory('formService', function($resource, $modal) {
  	}   
 });
 
+sentinelfApp.factory('formServiceForUpdate', function($resource, $modal) {
+    return {
+        findObjectById : function (listObject, idObject){
+            for(var i in listObject){
+                if(listObject[i].id == idObject) {return listObject[i]};
+            }
+            return listObject[0];
+        },
+        findObjectByIdWithIdRef : function (listObject, idObject, idRef){
+            for(var i in listObject){
+                if(listObject[i][idRef] == idObject) {return listObject[i]};
+            }
+            return listObject[0];
+        },
+        findObjectByCode : function (listObject, codeObject){
+            for(var i in listObject){
+                if(listObject[i].code == codeObject) {return listObject[i]};
+            }
+            return listObject[0];
+        },
+        popup: function (name, identity){
+            var modalInstance = $modal.open({
+                templateUrl: 'views/update.html',
+                controller: 'popupCtrl',
+                resolve: {
+                    name: function () {
+                        return name;
+                    },
+                    identity: function () {
+                        return identity;
+                    }
+                }
+            });
+            return modalInstance;
+        },
+        findInArray: function (arrayObj, id) {
+            for (var i in arrayObj) {
+                if (id == arrayObj[i].id)
+                    return i; 
+            }
+            return -1;
+        }
+    }   
+});
+
 sentinelfApp.controller('popupCtrl', ['$scope', '$modalInstance', 'name', 'identity', function($scope, $modalInstance, name, identity){
     $scope.name = name;
     $scope.identity = identity;
